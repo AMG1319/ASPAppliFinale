59d9750a1783727c1d7ece10001b57630c55380c
Marouane <marouane-_-hammani@outlook.be>
2022-01-05 04:55:17 +0100

    First Upload

---
+++ ASPAppliFinale/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ASPAppliFinale
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
