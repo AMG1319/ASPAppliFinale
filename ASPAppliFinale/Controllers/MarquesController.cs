c0f4a476c5a7e000e37a5eb5ad43dc1fbe2ff9cb
Marouane <marouane-_-hammani@outlook.be>
2022-01-05 05:23:46 +0100

    Scaffolding Marque OK

---
+++ ASPAppliFinale/Controllers/MarquesController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BDAutoASP.Models;
+
+namespace ASPAppliFinale.Controllers
+{
+    public class MarquesController : Controller
+    {
+        private BDContext db = new BDContext();
+
+        // GET: Marques
+        public ActionResult Index()
+        {
+            return View(db.Marques.ToList());
+        }
+        // GET: Marques/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Marques/Create
+        // Pour vous protéger des attaques par survalidation, activez les propriétés spécifiques auxquelles vous souhaitez vous lier. Pour 
+        // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IDMarque,MNom")] Marque marque)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Marques.Add(marque);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(marque);
+        }
+
+        // GET: Marques/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Marque marque = db.Marques.Find(id);
+            if (marque == null)
+            {
+                return HttpNotFound();
+            }
+            return View(marque);
+        }
+
+        // POST: Marques/Edit/5
+        // Pour vous protéger des attaques par survalidation, activez les propriétés spécifiques auxquelles vous souhaitez vous lier. Pour 
+        // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IDMarque,MNom")] Marque marque)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(marque).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(marque);
+        }
+
+        // GET: Marques/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Marque marque = db.Marques.Find(id);
+            if (marque == null)
+            {
+                return HttpNotFound();
+            }
+            return View(marque);
+        }
+
+        // POST: Marques/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Marque marque = db.Marques.Find(id);
+            db.Marques.Remove(marque);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
