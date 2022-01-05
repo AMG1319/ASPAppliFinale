8ff31775d669bfe60af8fdb643eff4abaebc0572
Marouane <marouane-_-hammani@outlook.be>
2022-01-05 05:08:31 +0100

    Migration OK

---
+++ ASPAppliFinale/Migrations/202201050405092_InitialCreate.cs
@@ -0,0 +1,79 @@
+﻿namespace ASPAppliFinale.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.TAnnonce",
+                c => new
+                    {
+                        IDAnnonce = c.Int(nullable: false, identity: true),
+                        IDProprio = c.Int(nullable: false),
+                        IDModel = c.Int(nullable: false),
+                        AAnnee = c.String(),
+                        AKilometrage = c.String(),
+                        ACouleur = c.String(),
+                        APrix = c.Single(nullable: false),
+                        AStatut = c.String(),
+                    })
+                .PrimaryKey(t => t.IDAnnonce)
+                .ForeignKey("dbo.TModel", t => t.IDModel, cascadeDelete: true)
+                .ForeignKey("dbo.TProprio", t => t.IDProprio, cascadeDelete: true)
+                .Index(t => t.IDProprio)
+                .Index(t => t.IDModel);
+            
+            CreateTable(
+                "dbo.TModel",
+                c => new
+                    {
+                        IDModel = c.Int(nullable: false, identity: true),
+                        IDMarque = c.Int(nullable: false),
+                        MNom = c.String(),
+                    })
+                .PrimaryKey(t => t.IDModel)
+                .ForeignKey("dbo.TMarque", t => t.IDMarque, cascadeDelete: true)
+                .Index(t => t.IDMarque);
+            
+            CreateTable(
+                "dbo.TMarque",
+                c => new
+                    {
+                        IDMarque = c.Int(nullable: false, identity: true),
+                        MNom = c.String(),
+                    })
+                .PrimaryKey(t => t.IDMarque);
+            
+            CreateTable(
+                "dbo.TProprio",
+                c => new
+                    {
+                        IDProprio = c.Int(nullable: false, identity: true),
+                        PNom = c.String(),
+                        PPrenom = c.String(),
+                        PNumTel = c.String(),
+                        PMail = c.String(),
+                        PMdp = c.String(),
+                    })
+                .PrimaryKey(t => t.IDProprio);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.TAnnonce", "IDProprio", "dbo.TProprio");
+            DropForeignKey("dbo.TAnnonce", "IDModel", "dbo.TModel");
+            DropForeignKey("dbo.TModel", "IDMarque", "dbo.TMarque");
+            DropIndex("dbo.TModel", new[] { "IDMarque" });
+            DropIndex("dbo.TAnnonce", new[] { "IDModel" });
+            DropIndex("dbo.TAnnonce", new[] { "IDProprio" });
+            DropTable("dbo.TProprio");
+            DropTable("dbo.TMarque");
+            DropTable("dbo.TModel");
+            DropTable("dbo.TAnnonce");
+        }
+    }
+}
---
+++ ASPAppliFinale/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace ASPAppliFinale.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BDAutoASP.Models.BDContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(BDAutoASP.Models.BDContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
